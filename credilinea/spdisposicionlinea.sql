9f9ab4c73bfc8197ae745fbe9089e425889d5532
hmotta <32286583+hmotta@users.noreply.github.com>
2018-04-24 17:17:40 -0500

    Ultimos Cambios de credilinea

--- credilinea/spdisposicionlinea.sql
+++ credilinea/spdisposicionlinea.sql
@@ -11,9 +11,9 @@ begin
 	select spssaldoadeudolinea into xsaldo_adeudo from spssaldoadeudolinea(pprestamoid);
 	select spssaldodisplinea into xsaldo_disponible from spssaldodisplinea(pprestamoid);
 
-  if xsaldo_disponible<pmontodispuesto then
-     raise exception 'La disposicion es mayor al saldo disponible de la linea';
-  end if;
+  --if xsaldo_disponible<pmontodispuesto then
+    -- raise exception 'La disposicion es mayor al saldo disponible de la linea';
+  --end if;
 
 return 1;
 end
