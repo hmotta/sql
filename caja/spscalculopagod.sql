31d51734801963dfcfa16b2f9217cf7fbef69e96
hmotta <32286583+hmotta@users.noreply.github.com>
2018-06-26 14:59:19 -0500

    funciones spscalculopago

--- caja/spscalculopagod.sql
+++ caja/spscalculopagod.sql
@@ -1,7 +1,9 @@
-CREATE or replace FUNCTION spscalculopago(integer) RETURNS SETOF tcalculopago
+CREATE or replace FUNCTION spscalculopagod(integer, date) RETURNS SETOF tcalculopago
     AS $_$
 declare
   lprestamoid alias for $1;
+  dfechapago  alias for $2;
+
   r tcalculopago%rowtype;
   rec record;
   amor record;
@@ -14,7 +16,7 @@ declare
   fmoratorio    numeric;
   fiva          numeric;
   ftotal        numeric;
-
+  
   fmontoprestamo   numeric;
   fsaldoprestamo   numeric;
   dfechaultimopago date;
@@ -37,26 +39,13 @@ declare
 
   dultimoabono date;
 
-  saplicareciprocidad char(1);
-
-  fsaldoact numeric;
-  fsaldocalculado numeric;
-
-  sclaveestadocredito char(3);
-
   gIVA numeric;
   gcobrardiainicial int4;
+  saplicareciprocidad char(1);
 
-  dfechaprestamo date;
+  dfechaprestamo date;  
 
   dfechacobroiva date;
-
-  dfecha2 date;
-
-  gmoratoriopormonto int4;
-  gdiasmoratoriopormonto int4;
-  gdiasanualesprestamo int4;
-
 
   fpagado numeric;
   idiastraspasoavencida numeric;
@@ -64,67 +53,20 @@ declare
   fmormenor numeric;
   fmormayor numeric;
   swmora numeric;
-  dfechapago date;
   dfecha_vencimiento date;
-  
-  sreferenciaprestamo character varying(18);
 
 begin
 
-  select iva,cobrardiainicial,aplicareciprocidad,fechacobroiva,moratoriopormonto,diasmoratoriopormonto,diasanualesprestamo
-    into gIVA,gcobrardiainicial,saplicareciprocidad,dfechacobroiva,gmoratoriopormonto,gdiasmoratoriopormonto,gdiasanualesprestamo
+  select iva,cobrardiainicial,aplicareciprocidad,fechacobroiva
+    into gIVA,gcobrardiainicial,saplicareciprocidad,dfechacobroiva
     from empresa where empresaid=1;
 
---raise notice 'Calculando';
-
-select fecha_otorga,fechaultimopago,trim(referenciaprestamo)
-  into dfechaprestamo,dfecha2,sreferenciaprestamo
-  from prestamos where prestamoid=lprestamoid;
-
-
-  if dfecha2<dfechaprestamo then
-
-    -- La fecha del ultimo pago es menor que la fecha del prestamo
-
-    update prestamos
-       set fechaultimopago=dfechaprestamo
-     where prestamoid=lprestamoid;
-
-  end if;
+select fecha_otorga into dfechaprestamo from prestamos where prestamoid=lprestamoid;
 
 if dfechaprestamo<dfechacobroiva then
   gIVA:=0;
-  saplicaiva:='N';
 end if;
 
-
--- Recalcular el saldo en base a pagos
-select p.saldoprestamo, p.montoprestamo-sum(m.haber),p.claveestadocredito
-  into fsaldoact,fsaldocalculado,sclaveestadocredito
-  from prestamos p, tipoprestamo tp, movicaja mc, movipolizas m
- where p.prestamoid = lprestamoid and
-       tp.tipoprestamoid = p.tipoprestamoid and
-       mc.prestamoid = p.prestamoid and
-       m.polizaid = mc.polizaid and
-       (m.cuentaid = tp.cuentaactivo or m.cuentaid = tp.cuentaactivoren)
-group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
-
-  fsaldoact:=coalesce(fsaldoact,0);
-  fsaldocalculado:=coalesce(fsaldocalculado,0);
-
-  raise notice ' Saldo Actual %  Saldo Calculado %',fsaldoact,fsaldocalculado;
-  if fsaldoact<>fsaldocalculado and not exists (select prestamoid from prestamos where referenciaprestamo = sreferenciaprestamo||'CAS-') then
-    raise notice 'Voy a updetear el saldo';
-    update prestamos
-       set saldoprestamo = fsaldocalculado
-     where prestamoid=lprestamoid;
-	 
-  end if;
-
---  select aplicareciprocidad
---    into saplicareciprocidad
---    from empresa
---   where empresaid=1;
 
   famortizacion := 0;
   fvencidas     := 0;
@@ -142,7 +84,7 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
   finteresminimo := coalesce(finteresminimo,0);
 
   select p.montoprestamo,p.saldoprestamo,p.tasanormal,p.tasa_moratoria,p.fecha_otorga,p.fecha_1er_pago,
-         tp.aplicaivaprestamo,p.calculonormalid,p.calculomoratorioid,tp.tantos,p.fecha_vencimiento
+         tp.aplicaivaprestamo,p.calculonormalid,p.calculomoratorioid,tp.tantos,p.fecha_vencimiento 
     into fmontoprestamo,fsaldoprestamo,ftasanormal,ftasa_moratoria,dfecha_otorga,dfecha_1er_pago,
          saplicaiva,icalculonormalid,icalculomoratorioid,itantos,dfecha_vencimiento
     from prestamos p, tipoprestamo tp
@@ -156,14 +98,6 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
    else
      freciprocidad := fmontoprestamo;
    end if;
-
-
---if dfechaprestamo<dfechacobroiva then
---  gIVA:=0;
---  saplicaiva:='N';
---end if;
-
-
 
 --
 -- Validar que el saldo insoluto del prestamo se encuentre calculado correctamente
@@ -187,6 +121,7 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
     from prestamos
    where prestamoid=lprestamoid;
 
+
    --
    -- Verificar la fecha de ultimo abono
    -- en caso de estar erronea correguirla
@@ -199,12 +134,12 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
           m.movipolizaid = mc.movipolizaid and
           mc.estatusmovicaja='A' and
           mc.tipomovimientoid='00' and
-          m.debe>0;
+          m.debe+m.haber>0;
 
    dultimoabono := coalesce(dultimoabono,dfechaultimopago);
 
-   --raise notice 'ult. abo. % - ult. pag. %',dultimoabono,dfechaultimopago;
-   if dultimoabono<>dfechaultimopago then
+--raise notice 'ult. abo. % - ult. pag. %',dultimoabono,dfechaultimopago;
+   if dultimoabono>dfechaultimopago then
 
      dfechaultimopago := dultimoabono;
      update prestamos
@@ -219,7 +154,7 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
    select coalesce(SUM(importeamortizacion-abonopagado),0.00)
      into famortizacion
      from amortizaciones
-    where prestamoid=lprestamoid and fechadepago=CURRENT_DATE and importeamortizacion-abonopagado>0;
+    where prestamoid=lprestamoid and fechadepago=dfechapago and importeamortizacion-abonopagado>0;
     
 --
 -- Buscar amortizaciones vencidas
@@ -227,9 +162,11 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
    select coalesce(SUM(round(importeamortizacion-abonopagado,2)),0.00)
      into fvencidas
      from amortizaciones
-    where prestamoid=lprestamoid and fechadepago<CURRENT_DATE and importeamortizacion-abonopagado>0;
-  
-  idiasint := CURRENT_DATE - dfechaultimopago;
+    where prestamoid=lprestamoid and fechadepago<dfechapago and importeamortizacion-abonopagado>0;
+
+--raise exception ' Vencidas % ',fvencidas;
+
+  idiasint := dfechapago - dfechaultimopago;
   fcapital := round( famortizacion + fvencidas, 2);
   
   if dfechaultimopago=dfecha_otorga then
@@ -246,24 +183,11 @@ group by p.saldoprestamo,p.montoprestamo,p.claveestadocredito;
 -- Calculo de interes Normal
 --
 
-  if ftasanormal>0 then
-    ftasanormal := tasareciprocidad(lprestamoid);
-  end if;
+  ftasanormal := tasareciprocidad(lprestamoid);
 
   if idiasint<0 then
     idiasint := 0;
   end if;
-raise notice 'updateando calculo................';
-
- if icalculonormalid=5 then 
-  update calculo
-     set saldoinsoluto = fsaldoinsoluto,
-         dias = idiasint,
-         tasaintnormal = ftasanormal,
-         tasaintmoratorio = ftasa_moratoria,
-	 montoprestamo=fmontoprestamo
-   where calculoid=icalculonormalid;
-else
 
   update calculo
      set saldoinsoluto = fsaldoinsoluto,
@@ -271,23 +195,22 @@ else
          tasaintnormal = ftasanormal,
          tasaintmoratorio = ftasa_moratoria
    where calculoid=icalculonormalid;
-end if;
 
   SELECT formula into sformula from calculo where calculoid=icalculonormalid;
 
   for rec in execute
       'SELECT round(' || sformula || ',2) as interes FROM calculo where calculoid='||icalculonormalid
   loop
-  if icalculonormalid in (5,4) then 
-     finteres=rec.interes;
-  else
+    -- Preguntar a Juan lo del redondeo
+    --raise exception '% -  % % % -  %',sformula,fsaldoinsoluto,idiasint,ftasanormal,rec.interes;
+
     if round(rec.interes,2)-trunc(round(rec.interes,2))>=0.50 then
       finteres := round(trunc(rec.interes)+1,2);
     else
       finteres := round(trunc(rec.interes),2);
     end if;
-  end if;
   end loop;
+
 
 --
 -- Calculo de interes moratorio
@@ -300,20 +223,16 @@ end if;
    where p.prestamoid = lprestamoid and
          t.tipoprestamoid = p.tipoprestamoid;
 
-  -- idiastraspasoavencida:=90;
-
-   --raise notice ' mora menor *** 1 ';
-
   swmora:=0;
   fmormenor :=0;
   fmormayor :=0;
-  dfechapago:=CURRENT_DATE;
 
---  if dfechaultimopago >=  dfecha_vencimiento then
+  --if dfechaultimopago >=  dfecha_vencimiento then
 
-  --  fmormayor:=fsaldoinsoluto*idiasint*ftasa_moratoria/100/360;
+    --fmormayor:=fsaldoinsoluto*idiasint*ftasa_moratoria/100/360;
 
-  --else
+ -- else
+
 
     for amor in
       select * from amortizaciones
@@ -321,16 +240,16 @@ end if;
       order by fechadepago
     loop
 
-      if fpagado<amor.importeamortizacion then
+    if fpagado<amor.importeamortizacion then
 
       --raise notice ' fpagado % amortizacion % --% --%--%',fpagado,amor.importeamortizacion,amor.fechadepago,fmormenor,dfechaultimopago;
 
       if amor.fechadepago<dfechapago then
         -- Calcular moratorio
-      --  if amor.fechadepago-dfechaultimopago<=idiastraspasoavencida then
+        --if amor.fechadepago-dfechaultimopago<=idiastraspasoavencida then
           --raise notice ' mora menor 1 ';
           if dfechapago-amor.fechadepago>0 then
-            
+
             if dfechaultimopago > amor.fechadepago then 
               idias:= dfechapago-dfechaultimopago;
             else 
@@ -344,53 +263,51 @@ end if;
 
             fmoratorio:=(amor.importeamortizacion-fpagado)*idias*ftasa_moratoria/100/360;
 
-            --raise notice ' mora menor 2 %  -- % -- idiasven %',idias,fmoratorio,idiastraspasoavencida;
+            --raise notice ' mora menor 2 %  -- %',idias,fmoratorio;
 
             if round(fmoratorio,2)-trunc(round(fmoratorio,2))>=0.50 then
-              fmormenor := fmormenor + round(fmoratorio+1,2);
-			  --  fmormenor := fmormenor + round(trunc(fmoratorio)+1,2);
+               fmormenor := fmormenor + round(fmoratorio+1,2);
             else
                fmormenor := fmormenor + round(fmoratorio,2);
-			  --   fmormenor := fmormenor + round(trunc(fmoratorio),2);
             end if;
 
             fpagado:=0;
                      
           end if;
+          ---inicio de calculo mora mal
        -- end if;
-		-- 31/05/2016 se comento esta parte ya que por cada amortizacion mayor le cobraba todo el saldo nuevamente
-        --if amor.fechadepago-dfechaultimopago>idiastraspasoavencida then
-            --raise notice ' mora mayor 1 * %', fmoratorio;
-          --  if dfechapago-amor.fechadepago>0 then
-              --raise noce ' mora mayor 2 ';
+
+       -- if amor.fechadepago-dfechaultimopago>idiastraspasoavencida then
+            --raise notice ' mora mayor 1 ';
+          -- if dfechapago-amor.fechadepago>0 then
+              --   raise notice ' mora mayor 2 ';
               -- Aqui se agrega el calculo especial de credimax
             
-            --     if swmora=0 and amor.fechadepago-dfechaultimopago > idiastraspasoavencida then
-            --          idias:= dfechapago-amor.fechadepago;
-            --          fmoratorio:=(fsaldoinsoluto)*idias*ftasa_moratoria/100/360;
-                      --raise notice ' mora mayor 2 ** % % %',fsaldoinsoluto,idias,fmoratorio;
-            --          swmora:=1;
+               --  if swmora=0 and amor.fechadepago-dfechaultimopago > idiastraspasoavencida then
+                 --     idias:= dfechapago-amor.fechadepago;
+                   --   fmoratorio:=(fsaldoinsoluto)*idias*ftasa_moratoria/100/360;
+                   --   swmora:=1;
                       -- Lo hacemos una vez y prendemos el switch
-            --      else
-            --          fmoratorio:=0;
-            --      end if;
+                --  else
+                 --     fmoratorio:=0;
+                --  end if;
 
-              --raise notice ' mora menor 3 %  -- % -- idiasven %',idias,fmoratorio,idiastraspasoavencida;
+                  --raise notice ' mora menor 3 %  -- %',idias,fmoratorio;
           
               -- Aqui termina 
 
-            --  if round(fmoratorio,2)-trunc(round(fmoratorio,2))>=0.50 then
+           --   if round(fmoratorio,2)-trunc(round(fmoratorio,2))>=0.50 then
               --   fmormayor := fmormayor+ round(trunc(fmoratorio)+1,2);
-            --  else
+             -- else
               --   fmormayor := fmormayor+ round(trunc(fmoratorio),2);
-            --  end if;
+             -- end if;
               
-              --fpagado:=0;
+            --  fpagado:=0;
 
-           --end if;
+          -- end if;
          
-       -- end if;
-
+        --end if;
+--fin de calculo mora mal
       else
         exit;
       end if;
@@ -400,30 +317,26 @@ end if;
     
     end loop;
 
- -- end if;
+  --end if;
 
   --if fmormayor = 0 and dfecha_vencimiento < dfechapago then
-    --  idias:= dfechapago-dfechaultimopago;
+      --idias:= dfechapago-dfechaultimopago;
       --idias:= dfechapago-dfecha_vencimiento;
-      --fmormayor:=(fsaldoinsoluto)*idias*ftasa_moratoria/100/360;
-      --raise notice ' mora menor 4 %  -- % -- idiasven %',idias,fmormayor,idiastraspasoavencida;
-  --end if;
+     -- fmormayor:=(fsaldoinsoluto)*idias*ftasa_moratoria/100/360;
+     -- raise notice ' mora menor 4 %  -- % -- idiasven %',idias,fmormayor,idiastraspasoavencida;
+ -- end if;
 
-	fmoratorio:=coalesce(fmormenor,0)+coalesce(fmormayor,0);
+ fmoratorio:=coalesce(fmormenor,0)+coalesce(fmormayor,0);
 
 
-  --if CURRENT_DATE-dfechaultimopago>gdiasmoratoriopormonto and gmoratoriopormonto=1 then
-    -- Calcular el moratrio en base al saldo
-    --fmoratorio:=(CURRENT_DATE-dfechaultimopago)*fsaldoinsoluto*ftasa_moratoria/100/gdiasanualesprestamo;
-  --end if;
-
+-- Aqui termina el procedimiento
+--
 
   -- No cobrar si la reciprocidad >= saldoprestamo
+
   if freciprocidad>=fsaldoprestamo and saplicareciprocidad='S' then
     fmoratorio := 0;
   end if;
-
-  -- Iva y pago minimo
 
 
 if dfechaprestamo<dfechacobroiva then
@@ -432,33 +345,29 @@ if dfechaprestamo<dfechacobroiva then
 end if;
 
 
-  if ftasanormal>0 then
-    if (1+gIVA)*(finteres+fmoratorio)<finteresminimo and dfechaultimopago<>CURRENT_DATE then
-      -- Tomar el interes minimo como interes normal    
-      finteres := finteresminimo;
-      fmoratorio := 0;
-    end if;
+  -- Iva y pago minimo
+
+  if (1+gIVA)*(finteres+fmoratorio)<finteresminimo and dfechaultimopago<>dfechapago then
+    -- Tomar el interes minimo como interes normal
+    finteres := finteresminimo;
+    fmoratorio := 0;
   end if;
 
   if saplicaiva='S' then
-
-      fiva := round( (finteres+fmoratorio)*gIVA , 2);
-
+    fiva := round( (finteres+fmoratorio)*gIVA , 2);
   else
-    if ftasanormal>0 then
-    if (finteres+fmoratorio)<finteresminimo and dfechaultimopago<>CURRENT_DATE then
+    if (finteres+fmoratorio)<finteresminimo and dfechaultimopago<>dfechapago then
       -- Tomar el interes minimo como interes normal
       finteres := finteresminimo;
       fmoratorio := 0;
       fiva:=0;
-    end if;
     end if;
   end if;
 
 
   ftotal := round(fcapital + finteres + fmoratorio + fiva, 2);
 
-  idiasint := CURRENT_DATE - dfechaultimopago;
+  idiasint := dfechapago - dfechaultimopago;
 
   if dfechaultimopago=dfecha_otorga then
     --
@@ -490,3 +399,6 @@ return;
 end
 $_$
     LANGUAGE plpgsql SECURITY DEFINER;
+
+
+ALTER FUNCTION public.spscalculopagod(integer, date) OWNER TO sistema;
