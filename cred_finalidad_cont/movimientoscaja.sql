3402e014b80270d34b4351de9fe4c6b6bbd4b980
Hugo Mota <arielmot@hotmail.com>
2019-09-03 17:50:43 -0500

    clasificacion contable prest

--- cred_finalidad_cont/movimientoscaja.sql
+++ cred_finalidad_cont/movimientoscaja.sql
@@ -45,7 +45,7 @@ begin
             from movicaja m, prestamos pr, cat_cuentas_tipoprestamo ct
            where m.movicajaid = r.movicajaid and
                  pr.prestamoid =  m.prestamoid and
-				(ct.tipoprestamoid = trim(pr.tipoprestamoid) and ct.clavefinalidad = pr.clavefinalidad and ct.renovado = pr.renovado)
+				(ct.tipoprestamoid = pr.tipoprestamoid and ct.clavefinalidad = pr.clavefinalidad and ct.renovado = pr.renovado)
 				 
                  
         loop
