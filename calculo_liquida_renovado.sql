c7b75dfde678aadaf0b61946bbb6af2d675419be
hmotta <arielmot@hotmail.com>
2019-10-09 00:17:22 +0000

    Devenga intereses 2

--- calculo_liquida_renovado.sql
+++ calculo_liquida_renovado.sql
@@ -8,29 +8,31 @@ CREATE TYPE rliquida_renovado AS (
   iva numeric,
   cobranza numeric,
   iva_cobranza numeric,
-  total numeric
+  total numeric,
+  tipoprestamoid character(3)
 );
 
 CREATE OR REPLACE FUNCTION calculo_liquida_renovado(integer)
-  RETURNS SET OF rliquida_renovado AS $BODY$
+  RETURNS SETOF rliquida_renovado AS 
+$BODY$
+  --Calcula el monto de capital, interes y demas accesorios que debe pagar, si se liquida con un renovado uno o mas creditos
 declare
   pprestamoid alias for $1;
-  nrevolvente integer;
   r rliquida_renovado;
 begin
-	for l in 
-		SELECT prestamoid,tipoprestamoid  from liquidaconrenovado where nuevoprestamoid=pprestamoid
+	for r in --se sacan todos los prestamos que tiene marcados por pagar
+		SELECT lr.prestamoid,t.revolvente,0 as capital,0 as interesnormal,0 as interesmoratorio,0 as iva,0 as cobranza, 0 as iva_cobranza,0 as total,t.tipoprestamoid  from liquidaconrenovado lr inner join prestamos p on (lr.prestamoid=p.prestamoid) inner join tipoprestamo t on (t.tipoprestamoid=p.tipoprestamoid) where lr.nuevoprestamoid=pprestamoid
 	loop
-		select revolvente into nrevolvente from tipoprestamo where tipoprestamoid=l.tipoprestamoid;
-		r.revolvente = nrevolvente;
-		if nrevolvente=0 then
+		if r.revolvente=0 then --Si es un credito ordinario
 			SELECT interes,moratorio,iva into r.interesnormal,r.interesmoratorio,r.iva FROM spscalculopago(l.prestamoid);
 			SELECT saldoprestamo into r.capital FROM prestamos where prestamoid=l.prestamoid;
-			select cobranza,ivacobranza into r.cobranza, r.iva_cobranza from verificacobranza(l.prestamoid,0);
+			select coalesce(cobranza,0),coalesce(ivacobranza,0) into r.cobranza, r.iva_cobranza from verificacobranza(l.prestamoid,0);
+			r.total = r.interesnormal+r.interesmoratorio+r.iva+r.capital+r.cobranza+r.iva_cobranza;
 			return next r;
-		else
-			SELECT select interes,moratorio,iva into r.interesnormal,r.interesmoratorio,r.iva from spscalculopago_linea(l.prestamoid,current_date);
+		else --Si es una linea revolvente
+			SELECT interes,moratorio,iva into r.interesnormal,r.interesmoratorio,r.iva from spscalculopago_linea(l.prestamoid,current_date);
 			select spssaldoadeudolinea into r.capital from spssaldoadeudolinea(l.prestamoid);
+			r.total = r.interesnormal+r.interesmoratorio+r.iva+r.capital;
 			return next r;
 		end if;
 	end loop;
--- spsprestamos.sql
+++ spsprestamos.sql
@@ -12,37 +12,39 @@ declare
   lsolicitudprestamoid int4;
   sreferenciaprestamo character varying(18);
   o record;
+  nrevolvente integer;
 begin
 
-  select prestamoid,trim(referenciaprestamo) into lprestamoid,sreferenciaprestamo
-    from prestamos where referenciaprestamo=preferenciaprestamo;
+  select p.prestamoid,trim(p.referenciaprestamo),t.revolvente into lprestamoid,sreferenciaprestamo,nrevolvente
+    from prestamos p inner join tipoprestamo t on (p.tipoprestamoid=t.tipoprestamoid) where p.referenciaprestamo=preferenciaprestamo;
+  
+  if nrevolvente=0 then
+	-- Recalcular el saldo en base a pagos
+	select p.saldoprestamo, p.montoprestamo-sum(m.haber)
+	  into fsaldoact,fsaldocalculado
+	  from movipolizas m, movicaja mc, prestamos p, cat_cuentas_tipoprestamo ct
+	 where p.prestamoid = lprestamoid and
+	--p.referenciaprestamo = preferenciaprestamo and
+		   ct.cat_cuentasid = p.cat_cuentasid and
+		   mc.prestamoid = p.prestamoid and
+		   m.polizaid = mc.polizaid and
+		   (m.cuentaid = ct.cta_cap_vig)
+	group by p.saldoprestamo,p.montoprestamo;
 
--- Recalcular el saldo en base a pagos
-select p.saldoprestamo, p.montoprestamo-sum(m.haber)
-  into fsaldoact,fsaldocalculado
-  from movipolizas m, movicaja mc, prestamos p, cat_cuentas_tipoprestamo ct
- where p.prestamoid = lprestamoid and
---p.referenciaprestamo = preferenciaprestamo and
-       ct.cat_cuentasid = p.cat_cuentasid and
-       mc.prestamoid = p.prestamoid and
-       m.polizaid = mc.polizaid and
-       (m.cuentaid = ct.cta_cap_vig)
-group by p.saldoprestamo,p.montoprestamo;
+	  raise notice ' Saldo Actual %  Saldo Calculado %',fsaldoact,fsaldocalculado;
+	  if fsaldoact<>fsaldocalculado and not exists (select prestamoid from prestamos where referenciaprestamo = sreferenciaprestamo||'CAS-') then
+		update prestamos
+		   set saldoprestamo = fsaldocalculado
+		 where referenciaprestamo=preferenciaprestamo;
 
-  raise notice ' Saldo Actual %  Saldo Calculado %',fsaldoact,fsaldocalculado;
-  if fsaldoact<>fsaldocalculado and not exists (select prestamoid from prestamos where referenciaprestamo = sreferenciaprestamo||'CAS-') then
-    update prestamos
-       set saldoprestamo = fsaldocalculado
-     where referenciaprestamo=preferenciaprestamo;
+		if fsaldocalculado=0 then
+		  update prestamos
+			 set claveestadocredito='002'
+		   where referenciaprestamo=preferenciaprestamo;
+		end if;
 
-    if fsaldocalculado=0 then
-      update prestamos
-         set claveestadocredito='002'
-       where referenciaprestamo=preferenciaprestamo;
-    end if;
-
-  end if;
-
+	  end if;
+	end if;
 
 -- Verificar que haya pasado los avales
    select solicitudprestamoid
