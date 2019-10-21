69acaa76da95573d9ea7759ad031e1b724cf5b3d
hmotta <arielmot@hotmail.com>
2019-10-21 17:41:21 -0500

    pld

--- spioperacionpld.sql
+++ spioperacionpld.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION spioperacionpld(integer,char,date,numeric,varchar,varchar,integer,integer,integer,integer)
+CREATE OR REPLACE FUNCTION spioperacionpld(integer,char,date,numeric,varchar,varchar,integer,integer,integer,varchar,varchar)
   RETURNS integer AS $BODY$
 declare
 	psocioid alias for $1;
@@ -10,27 +10,27 @@ declare
 	ptipo_operacion alias for $7;
 	ptipo_transaccion alias for $8;
 	pinstrumento_monetario alias for $9;
-	pmovicajaid alias for $10;
+	pfoliocaja alias for $10;
+	pparametroid alias for $11;
 	
 	ncuenta integer;
 	
 begin
 	--Sólo las operaciones con montos mayores o iguales a 100 
-	if pmonto<100 then
+	if pparametroid not in ('I17','I26') and pmonto<100 then
 		return 0;
 	end if;
 	
 	--Verificar que no exista una operacion con los mismos datos (socio,monto,fecha,motivo) 
-	select count(*) into ncuenta from operaciones_detectadas_pld where socioid=psocioid and monto=pmonto and fecha=pfecha and motivo=pmotivo;
+	select count(*) into ncuenta from operaciones_detectadas_pld where socioid=psocioid and parametroid=pparametroid and foliocaja=pfoliocaja;
 	if ncuenta>0 then
 		return 0;
 	end if;
 	
-	if pmovicajaid=0 then
-		pmovicajaid:=null;
-	end if;
 	
-	insert into operaciones_detectadas_pld (socioid,sucursal,fecha,monto,motivo,riesgo,tipo_operacion,tipo_transaccion,instrumento_monetario,movicajaid) values (psocioid,psucursal,pfecha,pmonto,pmotivo,priesgo,ptipo_operacion,ptipo_transaccion,pinstrumento_monetario,pmovicajaid);
+	
+	insert into operaciones_detectadas_pld (socioid,sucursal,fecha,monto,motivo,riesgo,tipo_operacion,tipo_transaccion,instrumento_monetario,fecha_deteccion,foliocaja,parametroid) values (psocioid,psucursal,pfecha,pmonto,pmotivo,priesgo,ptipo_operacion,ptipo_transaccion,pinstrumento_monetario,pfecha,pfoliocaja,pparametroid);
+	
 	
 	return currval('operaciones_detectadas_pld_operacionid_seq');
 	
